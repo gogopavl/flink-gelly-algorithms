98784075f1e84b4fad6e0b6eff643b61d73e79a4
pgogousis <pgogousis@gmail.com>
2019-07-02 15:36:50 +0100

    Adding single source shortest paths implementation

---
+++ SingleSourceShortestPath/src/main/java/flink/SSSPath.java
@@ -0,0 +1,61 @@
+package flink;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.graph.Edge;
+import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.Vertex;
+import org.apache.flink.graph.library.SingleSourceShortestPaths;
+import org.apache.flink.types.NullValue;
+
+/**
+ * Class implementing the single source shortest paths from a vertex on a graph
+ */
+public class SSSPath {
+
+	public static void main(String[] args) throws Exception {
+		// set up the batch execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		ParameterTool params = ParameterTool.fromArgs(args);
+
+		env.getConfig().setGlobalJobParameters(params); // Make params available to the web ui
+		
+		String edgeListFilePath = params.get("links", "Error");
+
+		long toc = System.nanoTime();
+		
+		Graph<Long, NullValue, NullValue> graph = Graph.fromCsvReader(edgeListFilePath, env).keyType(Long.class);
+
+		// Adding weights to graph edges for the shortest path algorithm
+		Graph<Long, NullValue, Double> weightedGraph = graph.mapEdges(new MapFunction<Edge<Long, NullValue>, Double>(){
+			@Override
+			public Double map(Edge<Long, NullValue> edge) throws Exception {
+				return 1.0;
+			}
+		});
+
+		long source = 1; // Source vertex
+		long destination = 10001; // Path destination
+		int iters = 20; // Maximum number of iterations
+
+		SingleSourceShortestPaths<Long, NullValue> singleSourceShortestPaths = new SingleSourceShortestPaths<>(source,iters);
+		DataSet<Vertex<Long, Double>> result = singleSourceShortestPaths.run(weightedGraph);
+
+		System.out.println("Vertex number of shortest paths: " + result.count());
+
+		result.filter(vertex -> vertex.getId().equals(destination)).print();
+
+		long tic = System.nanoTime();
+
+		long totalNanos = tic-toc;
+		double totalSeconds = (double) totalNanos / 1_000_000_000;
+
+		System.out.println("Total runtime: " + totalSeconds +" seconds");
+
+		// execute program
+		// env.execute("Flink Single Source Shortest Paths");
+	}
+}
